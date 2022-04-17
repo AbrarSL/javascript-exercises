6e9e8df98f75fe822d462fb38bbcc8803d14a310
AbrarSL <abrarsl2002@gmail.com>
2022-04-17 13:45:00 +0530

    Solution for test 1 in 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let stringArray = [];
+  for (let i = string.length - 1; i >= 0; i--) {
+    stringArray[string.length - i - 1] = string[i];
+  }
 
+  return stringArray.join("");
 };
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
