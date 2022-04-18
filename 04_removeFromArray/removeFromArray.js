635a0be84ffed7550f9369e2496d0e06a0ff5978
AbrarSL <abrarsl2002@gmail.com>
2022-04-18 11:35:08 +0530

    Pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, element) {
 
+  while (array.indexOf(element) != -1) {
+    array.splice(array.indexOf(element), 1);
+  }
+
+  return array;
 };
 
 // Do not edit below this line
