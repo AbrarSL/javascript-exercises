c26addfd6bd580fb0176525bc00d609fbf561530
AbrarSL <abrarsl2002@gmail.com>
2022-04-18 12:07:12 +0530

    Pass fifth test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,7 @@
 const sumAll = function(start, end) {
   let sum = 0;
 
+  if (typeof start != 'number' || typeof end != 'number') { return 'ERROR' }
   if (start < 0 || end < 0) { return 'ERROR' }
 
   if (start < end) {
