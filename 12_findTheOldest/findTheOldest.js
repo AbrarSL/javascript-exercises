57fe8d97cd0fe4fff68fff9940f776499c3457dd
AbrarSL <abrarsl2002@gmail.com>
2022-04-30 21:16:05 +0530

    Pass first test 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    if (oldest === null) {
+      oldest = person;
+    } else {
+      const oldestAge = (((oldest.yearOfDeath === undefined) ?
+      new Date().getFullYear() :
+      oldest.yearOfDeath) - oldest.yearOfBirth);
 
+      const personAge = (((person.yearOfDeath === undefined) ?
+      new Date().getFullYear() :
+      person.yearOfDeath) - person.yearOfBirth);
+
+      if (personAge > oldestAge) oldest = person;
+    }
+
+    return oldest;
+  }, null);
 };
 
 // Do not edit below this line
