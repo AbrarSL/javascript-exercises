3f4dd9780626ed357834df3250aa558a529ed1b6
AbrarSL <abrarsl2002@gmail.com>
2022-04-30 20:49:48 +0530

    Pass seventh test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,7 @@
 const fibonacci = function(n) {
   if (n < 0) return 'OOPS';
-  if (n === 1) return 1;
-  if (n === 0) return 0;
+  if (n == 1) return 1;
+  if (n == 0) return 0;
 
   return fibonacci(n - 1) + fibonacci(n - 2);
 };
